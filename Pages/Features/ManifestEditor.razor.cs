6faaae7d14672a15fff282a1f4d09c25b99be0ca
Daniel Henry <daniel.henry@epiforge.com>
2024-11-04 22:58:01 -0500

    ManifestEditor

--- Pages/Features/ManifestEditor.razor.cs
+++ Pages/Features/ManifestEditor.razor.cs
@@ -8,4 +8,13 @@ partial class ManifestEditor
         new("Features", "/features", icon: MaterialDesignIcons.Normal.FeatureSearch),
         new("Manifest Editor", "/features/manifest-editor", icon: MaterialDesignIcons.Normal.TagEdit)
     ];
+
+    readonly string scaffoldingMarkdown =
+        """
+        * The name of your mod
+        * Whether you marked that file as required
+        * What the component name of that file was
+        * What your hashing level was when you last updated the manifest
+        * How to find other files in your mod on your computer
+        """;
 }
