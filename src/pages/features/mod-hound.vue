4592b51bf6790a903f878134d35f7b402c1a2c49
amethystlilac <amethystlilac@users.noreply.github.com>
2025-05-14 18:23:37 -0230

    titles

--- src/pages/features/mod-hound.vue
+++ src/pages/features/mod-hound.vue
@@ -19,7 +19,7 @@
         {
             color: 'primary',
             icon: 'mdi-magnify-scan',
-            name: 'Any Mod, Available in Your Language',
+            name: 'Another Way to Keep Your Mods Folder Healthy',
             image: 'amphibia-frog-greenery-photo-md.png',
             description: `
 [Mod Hound](https://app.ts4modhound.com/) is a website created by [Lumpinou](https://www.patreon.com/lumpinou) so that creators can list their mods, and then players can have their mod folders scanned for things like outdated or broken mods, duplicate files, and missing requirements — a great resource for modders and players alike.
@@ -28,7 +28,7 @@
         {
             color: 'primary',
             icon: 'mdi-dog',
-            name: 'Easy, Fast, Straightforward',
+            name: 'At the Click of a Button',
             image: 'Nose.png',
             description: `
 Mod Hound has partnered with PlumbBuddy to let you run scans on your Mods folder from inside the PlumbBuddy app. A fast and easy way to find and fix problems! With the click of a button, get a report on the state of your mods, usually with convenient buttons to click to fix any issues.
