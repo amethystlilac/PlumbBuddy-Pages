7c3f328e3df72370997dc6c790338bfce009e9a0
Daniel Henry <daniel.henry@gmail.com>
2025-05-20 18:40:02 -0400

    more guide videos

--- src/pages/text-guides/creator-parlay.vue
+++ src/pages/text-guides/creator-parlay.vue
@@ -4,10 +4,7 @@
         :items="[{title: 'Guides', to: '/text-guides'}, {title: 'Parlay'}]"
     />
     <v-container>
-        <v-divider />
-        <div class="mx-auto" style="width: 560px;">
-			<iframe width="560" height="315" src="https://www.youtube.com/embed/T60_aToD_nM" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
-        </div>
+        <iframe src="https://www.youtube.com/embed/T60_aToD_nM" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen style="width: 100%; aspect-ratio: 16/9;"></iframe>
         <p>
             <center>Parlay</center>
             <center>Your Mods in Your Language</center>
