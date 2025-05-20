25181a3bc1be5e2005571e11dc15c2984d4c9288
Daniel Henry <daniel.henry@gmail.com>
2025-05-20 01:50:32 -0400

    various guide layout adjustments

--- src/pages/text-guides/creator-setup.vue
+++ src/pages/text-guides/creator-setup.vue
@@ -4,17 +4,14 @@
         :items="[{title: 'PlumbBuddy Setup Guide for Creators'}]"
     />
     <v-container>
-        <div class="mx-auto" style="width: 560px;">
-			<iframe width="560" height="315" src="https://www.youtube.com/embed/5qze0OZn1Rw?si=EB9Zh3d7VOFGiVqQ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
-        </div>
+        <iframe src="https://www.youtube.com/embed/5qze0OZn1Rw?si=EB9Zh3d7VOFGiVqQ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen style="width: 100%; aspect-ratio: 16/9;"></iframe>
         <p class="text-center">
             How to get PlumbBuddy up and running
         </p>
-        <v-divider />
-        <ContentMenu
-            :contents="contents"
-        />
     </v-container>
+    <ContentMenu
+        :contents="contents"
+    />
 </template>
 
 <script setup>
